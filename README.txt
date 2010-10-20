491eb387c5d5765801df48fac383e33c790c3cef
Josh <josh@hostelmarketing.com>
2010-10-20 03:45:47 -0700

    Initial import

---
+++ README.txt
@@ -0,0 +1,9 @@
+These are simple generators for random decimal and binary numbers, as well as card pairs.
+
+Examples:
+http://joshnotes.com/random/cards2.php
+http://joshnotes.com/random/decimal3.php
+http://joshnotes.com/random/binary3.php
+
+For questions about the scripts or memory training, ask me on Twitter:
+http://twitter.com/joshhostels
---
+++ cards.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <title>Random Cards for Memory Training</title>
+  <style type="text/css">
+  body {width: 90%;font-size:1.3em; padding: 0 20px;}
+  #data {font-family:Courier New, monospace;}
+  .red {color: red;}
+  .black {color: #000;}
+  </style>
+</head>
+<body>
+<h1>Random Card Pairs Generator</h1>
+<p>2652 card pairs for memory training. Copy into a word processor, add columns, and print. Reload the page (F5) to randomize again.</p>
+<p id="data">
+<?php
+
+$suits = array("&spades;","&hearts;","&diams;","&clubs;");
+$cards = array("A","2","3","4","5","6","7","8","9","10","J","Q","K");
+$pairs = array();
+
+foreach ($suits as $suit1) {
+	foreach ($cards as $card1) {
+		foreach ($suits as $suit2) {
+			foreach ($cards as $card2) {
+				
+				// set color of first card
+				if ($suit1 == '&spades;' || $suit1 == '&clubs;') {
+					$color = 'black';
+				} else {
+					$color = 'red';
+				}
+				
+				// assemble the first card
+				$set1 = '<span class="' . $color . '">' . $card1 . $suit1 . '</span>';
+
+				// set color of second card
+				if ($suit2 == '&spades;' || $suit2 == '&clubs;') {
+					$color = 'black';
+				} else {
+					$color = 'red';
+				}
+
+				// assemble the second card
+				$set2 = '<span class="' . $color . '">' . $card2 . $suit2 . '</span>';
+				
+				// removing 52 duplicates, insert the assembled pairs into the array
+				// e.g., there aren't two Ace of Spades in one deck
+				if ($set1 != $set2) {
+					// fill the array with pairs
+					$pairs[] = "$set1 $set2";
+				}
+			}
+		}
+	}
+}
+
+// randomize
+shuffle($pairs);
+
+// print the output
+foreach ($pairs as $pair) {
+	echo $pair . "<br />\n";
+}
+
+?>
+</p>
+
+</body>
+</html>
