2de65e60a90c6d0b8f196e7423bdd5d71b25ebfc
Josh <josh@hostelmarketing.com>
2010-10-23 15:46:16 -0700

    mods

--- memory_system.php
+++ memory_system.php
@@ -15,46 +15,27 @@
 <?php
 
 $first_letters = array(
-	0  => (array ( phones  => array('S','Z'), decimal => 0, binary => 0, suit=> array (suit1 => 's', suit2 => 's') )),
-	1  => (array ( phones  => array('T'), decimal => 1, binary => 1, suit => array (suit1 => 'c', suit2 => 'd') )),
-	2  => (array ( phones  => array('n'), decimal => 2, binary => 2, suit => array (suit1 => 'c', suit2 => 'h') )),
-	3  => (array ( phones  => array('m'), decimal => 3, binary => 3, suit => array (suit1 => 'c', suit2 => 's') )),
-	4  => (array ( phones  => array('r','ر‎'), decimal => 4, binary => 4, suit => array (suit1 => 'd', suit2 => 'c') )),
-	5  => (array ( phones  => array('L'), decimal => 5, binary => 5, suit => array (suit1 => 'd', suit2 => 'h') )),
-	6  => (array ( phones  => array('b'), decimal => 6, binary => 6, suit => array (suit1 => 'd', suit2 => 's') )),
-	7  => (array ( phones  => array('k','ח','ق‎'), decimal => 7, binary => 7, suit => array (suit1 => 'c', suit2 => 'c') )),
-	8  => (array ( phones  => array('f','v'), decimal => 8, binary => 8, suit => array (suit1 => 'h', suit2 => 'c') )),
-	9  => (array ( phones  => array('p'), decimal => 9, binary => 9, suit => array (suit1 => 'h', suit2 => 'd') )),
-	10 => (array ( phones  => array('g','y','Γ','غ‎','ع'), decimal => NULL, binary => 10, suit => array (suit1 => 'h', suit2 => 's') )),
-	11 => (array ( phones  => array('h'), decimal => NULL, binary => 11, suit => array (suit1 => 'h', suit2 => 'h') )),
+	0  => (array ( phones  => array('S','Z'), decimal => 0, binary => 0, cards => array (suit1 => 's', suit2 => 's') )),
+	1  => (array ( phones  => array('T'), decimal => 1, binary => 1, cards => array (suit1 => 'c', suit2 => 'd') )),
+	2  => (array ( phones  => array('n'), decimal => 2, binary => 2, cards => array (suit1 => 'c', suit2 => 'h') )),
+	3  => (array ( phones  => array('m'), decimal => 3, binary => 3, cards => array (suit1 => 'c', suit2 => 's') )),
+	4  => (array ( phones  => array('r'), decimal => 4, binary => 4, cards => array (suit1 => 'd', suit2 => 'c') )),
+	5  => (array ( phones  => array('L'), decimal => 5, binary => 5, cards => array (suit1 => 'd', suit2 => 'h') )),
+	6  => (array ( phones  => array('b'), decimal => 6, binary => 6, cards => array (suit1 => 'd', suit2 => 's') )),
+	7  => (array ( phones  => array('k'), decimal => 7, binary => 7, cards => array (suit1 => 'c', suit2 => 'c') )),
+	8  => (array ( phones  => array('f','v'), decimal => 8, binary => 8, cards => array (suit1 => 'h', suit2 => 'c') )),
+	9  => (array ( phones  => array('p'), decimal => 9, binary => 9, cards => array (suit1 => 'h', suit2 => 'd') )),
+	10 => (array ( phones  => array('g','y'), decimal => NULL, binary => 10, cards => array (suit1 => 'h', suit2 => 's') )),
+	11 => (array ( phones  => array('h'), decimal => NULL, binary => 11, cards => array (suit1 => 'h', suit2 => 'h') )),
 	12 => (array ( phones  => array('sk','sn','sm'), decimal => NULL, binary => 12, suit => array (suit1 => 's', suit2 => 'c') )),
-	13 => (array ( phones  => array('st','sp'), decimal => NULL, binary => 13, suit => array (suit1 => 's', suit2 => 'd') )),
+	13 => (array ( phones  => array('st','sp'), decimal => NULL, binary => 13, cards => array (suit1 => 's', suit2 => 'd') )),
 	14 => (array ( phones  => array('sh','sl','sw','j','ch'), decimal => NULL, binary => 14, suit => array (suit1 => 's', suit2 => 'h') )),
-	15 => (array ( phones  => array('d'), decimal => NULL, binary => 15, suit => array (suit1 => 'd', suit2 => 'd') )),
-	16 => (array ( phones  => NULL, decimal => NULL, binary => NULL, suit => NULL )),
-	17 => (array ( phones  => NULL, decimal => NULL, binary => NULL, suit => NULL )),
-	18 => (array ( phones  => NULL, decimal => NULL, binary => NULL, suit => NULL ))
+	15 => (array ( phones  => array('d'), decimal => NULL, binary => 15, cards => array (suit1 => 'd', suit2 => 'd') )),
+	16 => (array ( phones  => NULL, decimal => NULL, binary => NULL, cards => NULL )),
+	17 => (array ( phones  => NULL, decimal => NULL, binary => NULL, cards => NULL )),
+	18 => (array ( phones  => NULL, decimal => NULL, binary => NULL, cards => NULL ))
 );
 //echo "Letters: $first_letters[0][phones][0], $first_letters[0][phones][1] | $first_letters[1]"; // S
-
-
-$cycles = count($first_letters); 
-echo "<ol>";
-for ($row = 0; $row < $cycles; $row++)
-{
-    echo "<li><b>The row number $row</b>";
-    echo "<ul>";
-
-    for ($col = 0; $col < 3; $col++)
-    {
-        echo "<li>".$first_letters[$row][$col]."</li>";
-    }
-
-    echo "</ul>";
-    echo "</li>";
-}
-echo "</ol>";
-
 
 $middle_letters = array(
 	0  => 'o',
@@ -98,20 +79,62 @@ $final_letters = array(
 	17 => 'g',
 	18 => 'd'
 );
+
+$full_set = array(first => $first_letters, middle => $middle_letters, last => $final_letters);
 ?>
 <pre>
 
 <?php
 
+echo '<hr /><h2>Extract</h2>';
+$cycles = count($first_letters);
+
+//echo $first_letters[0][phones][0] .'<p></p>'; // $first_letters[0][phones][1]; //| $first_letters[1];
+for ( $row = 0; $row < $cycles; $row++ ) {
+    while ( list( $key, $value ) = each( $first_letters[ $row ] ) ) {
+        if(is_array($value)) {
+            foreach ($value as $fdsa) {
+                echo "|$fdsa";
+            }
+        }  else {
+            echo "|$value";
+        }
+    }
+ echo '|<br />';
+}
+
+
+//print_r($first_letters);
+//print_r($middle_letters);
+//print_r($final_letters);
+
+echo '<h2>$full_set</h2>';
+print_r($full_set);
+
+echo '<hr /><h2>Cycles</h2>';
+$cycles = count($first_letters);
+echo "<p>Number: $cycles</p>";
+echo "<ol>";
+for ($row = 0; $row < $cycles; $row++)
+{
+    echo "<li><b>The row number $row</b>";
+    echo "<ul>";
+
+    for ($col = 0; $col < 3; $col++)
+    {
+    	//$first_letters[0][phones][0], $first_letters[0][phones][1] | $first_letters[1]
+        echo "<li>".$first_letters[$row][phones][$col]."</li>";
+    }
+
+    echo "</ul>";
+    echo "</li>";
+}
+echo "</ol>";
+
+
 foreach ($first_letters as $first_letter) {
 	//echo $first_letter[1];
 }
-
-print_r($first_letters);
-
-print_r($middle_letters);
-
-print_r($final_letters);
 ?>
 </pre>
 
@@ -138,14 +161,14 @@ foreach ($suits as $suit1) {
 	foreach ($cards as $card1) {
 		foreach ($suits as $suit2) {
 			foreach ($cards as $card2) {
-				
+
 				// set color of first card
 				if ($suit1 == '&spades;' || $suit1 == '&clubs;') {
 					$color = 'black';
 				} else {
 					$color = 'red';
 				}
-				
+
 				// assemble the first card
 				$set1 = '<span class="' . $color . '">' . $card1 . $suit1 . '</span>';
 
@@ -158,7 +181,7 @@ foreach ($suits as $suit1) {
 
 				// assemble the second card
 				$set2 = '<span class="' . $color . '">' . $card2 . $suit2 . '</span>';
-				
+
 				// removing 52 duplicates, insert the assembled pairs into the array
 				// e.g., there aren't two Ace of Spades in one deck
 				if ($set1 != $set2) {
@@ -184,3 +207,25 @@ foreach ($pairs as $pair) {
 
 </body>
 </html>
+<?php
+/*
+ * this works
+
+echo '<hr /><h2>Extract</h2>';
+$cycles = count($first_letters);
+
+//echo $first_letters[0][phones][0] .'<p></p>'; // $first_letters[0][phones][1]; //| $first_letters[1];
+for ( $row = 0; $row < $cycles; $row++ ) {
+    while ( list( $key, $value ) = each( $first_letters[ $row ] ) ) {
+        if(is_array($value)) {
+            foreach ($value as $fdsa) {
+                echo "|$fdsa";
+            }
+        }  else {
+            echo "|$value";
+        }
+    }
+ echo '|<br />';
+}
+ */
+ *?>
